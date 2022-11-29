5c889065874b7535c3abc29b3bc5e577e04de998
Vova <weborsha@gmail.com>
2022-11-29 15:03:15 +0300

    Initial commit

--- Backend/app/controllers/user_wallets_check.controller.js
+++ Backend/app/controllers/user_wallets_check.controller.js
@@ -1,15 +1,19 @@
 const axios = require('axios');
 const db = require("../models");
 const DepositHistoryEth = db.deposit_history_eth;
+const DepositHistoryBnb = db.deposit_history_bnb;
 
 exports.depositEthCheck = async (req, res) => {
     if (req.body.confirmed === true && req.body.streamId === '4abb4dd9-ca95-49c2-84ac-23ce8b60ebe4') {
         let token_name = '';
+        let model_name = '';
         if (req.body.chainId === '0x61' || req.body.chainId === '0x38') {
             token_name = 'binancecoin';
+            model_name = DepositHistoryBnb;
         }
         if (req.body.chainId === '0x1') {
             token_name = 'ethereum';
+            model_name = DepositHistoryEth;
         }
         let token_price = axios.get('https://api.coingecko.com/api/v3/simple/price?ids=' + token_name + '&vs_currencies=usd');
 
@@ -23,9 +27,10 @@ exports.depositEthCheck = async (req, res) => {
         axios.all([token_price, tx_check])
             .then(
                 axios.spread(async (...responses) => {
-                    let token_rate = responses[0].data.binancecoin.usd;
+                    console.log(token_name);
+                    let token_rate = responses[0].data[token_name]['usd'];
                     let amount = req.body.txs[0].value / 1000000000000000000;
-                    await DepositHistoryEth.create({
+                    await model_name.create({
                         deposit_address: responses[1].data.to_address,
                         amount: amount,
                         token_rate: token_rate
