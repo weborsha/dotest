d2915f0fb5adaa323dc6f8d0a13e05476e8d6175
Vova <weborsha@gmail.com>
2022-11-29 07:12:51 +0300

    Initial commit

--- Backend/app/controllers/user_wallets_check.controller.js
+++ Backend/app/controllers/user_wallets_check.controller.js
@@ -4,30 +4,37 @@ const DepositHistoryEth = db.deposit_history_eth;
 
 exports.depositEthCheck = async (req, res) => {
     if (req.body.confirmed === true && req.body.streamId === '4abb4dd9-ca95-49c2-84ac-23ce8b60ebe4') {
+        let token_name = '';
+        if (req.body.chainId === '0x61' || req.body.chainId === '0x38') {
+            token_name = 'binancecoin';
+        }
+        if (req.body.chainId === '0x1') {
+            token_name = 'ethereum';
+        }
+        let token_price = axios.get('https://api.coingecko.com/api/v3/simple/price?ids=' + token_name + '&vs_currencies=usd');
 
-        let bnb_price = axios.get('https://api.coingecko.com/api/v3/simple/price?ids=binancecoin&vs_currencies=usd');
-        console.log(bnb_price);
-
-        let config = {
+        let tx_check = axios({
             method: 'get',
             url: 'https://deep-index.moralis.io/api/v2/transaction/' + req.body.txs[0].hash + '?chain=' + req.body.chainId,
             headers: {
                 'X-API-Key': 'AZV0avIB7qdAVVLrHtJCbnPM1yVBp5WuprODCrZ685LyVMbXaTVGZUQfu7B3RG5p'
             }
-        };
-        axios(config)
-            .then(async function (response) {
-                //console.log(JSON.stringify(response.data));
-                let amount = req.body.txs[0].value / 1000000000000000000;
-                await DepositHistoryEth.create({
-                    user_address: response.from_address,
-                    deposit_address: '43gg43h43h',
-                    amount: amount,
-                    eth_rate: '10.15454'
-                });
-            })
-            .catch(function (error) {
-                console.log(error);
+        });
+        axios.all([token_price, tx_check])
+            .then(
+                axios.spread(async (...responses) => {
+                    let token_rate = responses[0].data.binancecoin.usd;
+                    let amount = req.body.txs[0].value / 1000000000000000000;
+                    await DepositHistoryEth.create({
+                        deposit_address: responses[1].data.to_address,
+                        amount: amount,
+                        token_rate: token_rate
+                    });
+                })
+            )
+            .catch(errors => {
+                // react on errors.
+                console.error(errors);
             });
         res.status(200).send("ok");
     } else {
