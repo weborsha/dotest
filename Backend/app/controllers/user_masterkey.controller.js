82a0048ee9823f25c9e4272e5200804b4cabb050
Vova <weborsha@gmail.com>
2023-01-09 06:54:01 +0300

    Add MASTERKEY_SECRET

--- Backend/app/controllers/user_masterkey.controller.js
+++ Backend/app/controllers/user_masterkey.controller.js
@@ -6,33 +6,33 @@ const CryptoJS = require("crypto-js");
 const User = db.user;
 const UserKeys = db.user_keys;
 
-const user_master_key_decrypt = async (seed, user_id) => {
-  let pub_key_from = await User.findAll({
-    attributes: ['pubkey'],
-    where: {
-      id: user_id
-    }
-  });
-  let key = CryptoJS.enc.Utf8.parse(pub_key_from[0]['pubkey']);
+const user_master_key_decrypt = async (seed) => {
+  let key = CryptoJS.enc.Utf8.parse(process.env.MASTERKEY_SECRET);
   let decrypted = CryptoJS.AES.decrypt({ciphertext: CryptoJS.enc.Hex.parse(seed)}, key, {
     mode: CryptoJS.mode.ECB,
     padding: CryptoJS.pad.ZeroPadding
   });
   //let master_key = await decrypted.toString(CryptoJS.enc.Utf8);
-  let master_key = await CryptoJS.enc.Utf8.stringify(decrypted);
   //console.log(master_key[0]);
-  return master_key;
+  return await CryptoJS.enc.Utf8.stringify(decrypted);
 }
 
 
 exports.userMaster = async (req, res) => {
-  const master_key = await UserKeys.findAll({
+  const userAddress = await User.findAll({
+    attributes: ['address'],
     where: {
       id: req.userId
     }
   });
 
-  let ever_privateSeed_decrypt = await user_master_key_decrypt(master_key[0].seed, req.userId)
+  const master_key = await UserKeys.findAll({
+    where: {
+      address: userAddress[0]['address']
+    }
+  });
+
+  let ever_privateSeed_decrypt = await user_master_key_decrypt(master_key[0].seed)
 
   ever_privateSeed_decrypt = ever_privateSeed_decrypt.toString()
   //console.log(master_key[0].seed);
